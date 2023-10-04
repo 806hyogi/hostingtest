305f3b4817d1484e67cb8be41c7313b96fb3a05c
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-10-04 19:24:13 +0900

    속도 1

--- geolocation.js
+++ geolocation.js
@@ -29,32 +29,36 @@ if (navigator.geolocation) {
       // speed(m/s)
       var speed = distanceDiff / timeDiff;
 
-     document.getElementById('speed').innerText = 'Speed : ' + Math.round(speed *3.6 )+ ' km/h';
-      
+      document.getElementById('speed').innerText = 'Speed : ' + Math.round(speed * 3.6) + ' km/h';
+
+      lastTime = position.timestamp;
+      lastPosition = currentPosition;
+
+
     } else {
-       lastTime= position.timestamp;
-       lastPosition= currentPosition;
-       
-       document.getElementById('speed').innerText ='Speed: Calculating...';
-       
-     }
+      lastTime = position.timestamp;
+      lastPosition = currentPosition;
 
-     // 모든 포인트 범위 안에 들어왔을때 사용자를 체크한다.
-     pointPositions.forEach(function(pointPosition) {
-       if (kakao.maps.geometry.distance(currentPosition, pointPosition) <= 1000) {
-         alert("포인트 +1");
-         window.close(); // 페이지 자동 종료
-       }
-     });
+      document.getElementById('speed').innerText = 'Speed: 0 km/h';
 
-   }, function(error){
-     console.log("Error occurred in watchPosition.");
-     console.log(error);
-   }, { 
-     enableHighAccuracy: true,
-     maximumAge: 30000,
-     timeout: 27000
-   });
-} else{
+    }
+
+    // 모든 포인트 범위 안에 들어왔을때 사용자를 체크한다.
+    pointPositions.forEach(function (pointPosition) {
+      if (kakao.maps.geometry.distance(currentPosition, pointPosition) <= 1000) {
+        alert("포인트 +1");
+        window.close(); // 페이지 자동 종료
+      }
+    });
+
+  }, function (error) {
+    console.log("Error occurred in watchPosition.");
+    console.log(error);
+  }, {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000
+  });
+} else {
   console.log("Geolocation API가 지원하지 않습니다.");
 }
