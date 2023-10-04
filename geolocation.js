f8c106c9bf4819f7edd737012499032f5d8a9bcf
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-10-04 19:07:26 +0900

    위치

--- geolocation.js
+++ geolocation.js
@@ -1,6 +1,8 @@
 // Geolocation과 관련된 코드를 이 파일로 이동합니다.
 
 if (navigator.geolocation) {
+  var lastTime = null; // 시간 측정을 위한 변수
+
   navigator.geolocation.watchPosition(function (position) {
     var lat = position.coords.latitude,
       lon = position.coords.longitude;
@@ -17,9 +19,25 @@ if (navigator.geolocation) {
     if (lastPosition != null) {
       let angle = calculateAngle(lastPosition, currentPosition);
       marker = rotateMarker(marker, angle);
+
+      // 시간 차이(초)
+      var timeDiff = (position.timestamp - lastTime) / 1000;
+
+      // 거리 차이(km)
+      var distanceDiff = kakao.maps.geometry.distance(lastPosition, currentPosition) / 1000;
+
+      // speed(m/s)
+      var speed = distanceDiff / timeDiff;
+
+     document.getElementById('speed').innerText = 'Speed : ' + Math.round(speed *3.6 )+ ' km/h';
+      
     } else {
-      lastPosition = currentPosition;
-    }
+       lastTime= position.timestamp;
+       lastPosition= currentPosition;
+       
+       document.getElementById('speed').innerText ='Speed: Calculating...';
+       
+     }
 
      // 모든 포인트 범위 안에 들어왔을때 사용자를 체크한다.
      pointPositions.forEach(function(pointPosition) {
