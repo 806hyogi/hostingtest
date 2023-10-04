c6323ad7936038c9b957cd3a8a63c59d32b76cc3
806hyogi <rhkdgur120@naver.com>
2023-10-05 01:55:56 +0900

    d

--- geolocation.js
+++ geolocation.js
@@ -16,27 +16,25 @@ if (navigator.geolocation) {
       lastPosition = currentPosition;
     }
 
-     // 모든 포인트 범위 안에 들어왔을때 사용자를 체크한다.
-     pointPositions.forEach(function(pointPosition) {
-       if (kakao.maps.geometry.distance(currentPosition, pointPosition) <= 1000) {
-         alert("포인트 +1");
-         window.close(); // 페이지 자동 종료
-       }
-     });
+    // 모든 포인트 범위 안에 들어왔을때 사용자를 체크한다.
+    pointPositions.forEach(function (pointPosition) {
+      if (kakao.maps.geometry.distance(currentPosition, pointPosition) <= 1000) {
+        alert("포인트 +1");
+        window.close(); // 페이지 자동 종료
+      }
+    });
 
-     // 10초 뒤에 지도의 중심을 현재 위치로 설정합니다.
-     setTimeout(function() { 
-        //map.setCenter(currentPosition); 
-     }, 10000);
+    map.setCenter(currentPosition);
 
-   }, function(error){
-     console.log("Error occurred in watchPosition.");
-     console.log(error);
-   }, { 
-     enableHighAccuracy: true,
-     maximumAge: 30000,
-     timeout: 27000
-   });
-} else{
+
+  }, function (error) {
+    console.log("Error occurred in watchPosition.");
+    console.log(error);
+  }, {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000
+  });
+} else {
   console.log("Geolocation API가 지원하지 않습니다.");
 }
