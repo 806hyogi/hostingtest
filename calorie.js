b023fa409f83ee0779f64c24410686372562c28e
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-10-06 20:32:58 +0900

    dd

--- calorie.js
+++
@@ -1,7 +0,0 @@
-const calorieBurnRate=10;
-
-if(isDriving){  
-     const minutesElapsed=elapsedTime/(1000*60);       
-     const burnedCalories=minutesElapsed*calorieBurnRate ;
-     document.getElementById('caloriesBurned').innerText=burnedCalories.toFixed(2); 
-}
